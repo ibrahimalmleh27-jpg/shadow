afd2f004482c90aa9d41ce6c16a82158f8780fdb
ibrahimalmleh27-jpg <ibrahimalmleh27@gmail.com>
2026-05-10 13:47:23 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,9 +5,13 @@ import fs from "fs"
 
 const app = express()
 app.use(cors())
+
+// 📁 فولدرات
 app.use(express.static("public"))
 app.use("/uploads", express.static("uploads"))
+app.use("/img", express.static("img")) // 👈 مهم عشان الصورة
 
+// 📂 رفع ملفات
 const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, "uploads/"),
   filename: (req, file, cb) => cb(null, Date.now() + "-" + file.originalname)
@@ -17,16 +21,18 @@ const upload = multer({ storage })
 
 const DB_FILE = "images.json"
 
+// 📌 قراءة
 function loadDB() {
   if (!fs.existsSync(DB_FILE)) return []
   return JSON.parse(fs.readFileSync(DB_FILE))
 }
 
+// 📌 حفظ
 function saveDB(data) {
   fs.writeFileSync(DB_FILE, JSON.stringify(data, null, 2))
 }
 
-// 📤 رفع ملف
+// 📤 رفع
 app.post("/api/upload", upload.single("file"), (req, res) => {
   let db = loadDB()
 
@@ -47,10 +53,11 @@ app.get("/api/gallery", (req, res) => {
   res.json(loadDB())
 })
 
-// 🌐 الصفحة الرئيسية
+// 🌐 الصفحة
 app.get("/", (req, res) => {
   res.sendFile(process.cwd() + "/public/index.html")
 })
 
+// 🚀 تشغيل
 const PORT = process.env.PORT || 3000
 app.listen(PORT, () => console.log("🔥 S7ADOW RUNNING"))
